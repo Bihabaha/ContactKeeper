28c90fd4968d3fab3e5f900701959646a77f35a0
Brahim Knioui <brahimknioui@Brahims-MacBook-Air.local>
2023-10-24 21:13:48 +0300

    Controllers

---
+++ backend/controllers/contactControllers.js
@@ -0,0 +1,67 @@
+//mongoose
+const mongoose = require("mongoose")
+//contatct schema
+const Contact = require("../models/contactSchema")
+
+
+//Get all contacts
+const getContacts = async(req,res)=>{
+ const contact = await Contact.find({}).sort({createdAt:-1})
+ res.status(200).json(contact)
+}
+//Post all contacts
+const postContacts = async(req,res)=>{
+const {fullName,address,phoneNumber}= req.body
+//add to db
+try{
+ const contact = await Contact.create({fullName,address,phoneNumber})
+ res.status(200).json(contact)
+}catch(error){
+res.status(400).json({error:error.message})
+}
+
+}
+//Get singleContact
+const getSingleContact = async(req,res)=>{
+const {id}=req.params
+if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error:"No such contact"})
+}
+const contact = await Contact.findById(id)
+if(!contact){
+    return res.status(404).json({error:"No such contact"})
+}
+res.status(200).json(contact)
+}
+//Get all contacts
+const deleteContact = async(req,res)=>{
+    const {id}=req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No such contact"})
+    }
+    const contact = await Contact.findOneAndDelete({_id:id})
+    if(!contact){
+        return res.status(400).json({error:"No such contact"})
+    }
+    res.status(200).json(contact)
+}
+//Get all contacts
+const updateContacts = async(req,res)=>{
+    const {id}=req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No such contact"})
+    }
+    const contact = await Contact.findOneAndUpdate({_id:id},{...req.body})
+    if(!contact){
+        return res.status(400).json({error:"No such contact"})
+    }
+    res.status(200).json(contact)
+}
+
+module.exports={
+    postContacts,
+    getContacts,
+    getSingleContact,
+    deleteContact,
+    updateContacts
+}
--- backend/routes/contactRoute.js
+++ backend/routes/contactRoute.js
@@ -4,35 +4,18 @@ const express = require("express")
 const Contact = require("../models/contactSchema")
 //express router
 const router = express.Router()
-
-
+//controllers
+const { postContacts, getContacts, getSingleContact, deleteContact, updateContacts}= require("../controllers/contactControllers")
 //post a contact
-router.post("/",async(req,res)=>{
-const{fullName,address,phoneNumber}=req.body
-try{
-//add to db
-const contact = await Contact.create({fullName,address,phoneNumber})
-res.status(200).json(contact)
-}catch(error){
-res.status(400).json({error:error.message})
-}
-})
+router.post("/",postContacts)
 //get all contacts
-router.get("/",(req,res)=>{
-    res.json({msg:"GET all contact"})
-})
+router.get("/",getContacts)
 //get a single contacts
-router.get("/:id",(req,res)=>{
-    res.json({msg:"GET single all contact"})
-})
+router.get("/:id",getSingleContact)
 //delete all contacts
-router.delete("/:id",(req,res)=>{
-    res.json({msg:"remove single contact"})
-})
+router.delete("/:id",deleteContact)
 //update all contacts
-router.patch("/:id",(req,res)=>{
-    res.json({msg:"update single contact"})
-})
+router.patch("/:id",updateContacts)
 
 
 module.exports=router
