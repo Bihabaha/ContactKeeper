8ea3da36b2b4d6de99cf73241f83f575c8d8d830
Brahim Knioui <brahimknioui@Brahims-MacBook-Air.local>
2023-11-01 17:15:22 +0200

    json webtoken signup which contains header,payload and signature

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,7 +1,10 @@
 const User = require("../models/userSchema")
-
-
-
+//json webtoken
+const jwt = require("jsonwebtoken")
+//
+const createToken= (_id)=>{
+return jwt.sign({_id},process.env.SECRET,{expiresIn:"3d"})
+}
 //loginuser
 const loginUser=async(req,res)=>{
     res.json({msg:"login user"})
@@ -12,7 +15,9 @@ const signupUser=async(req,res)=>{
    const {email,password}= req.body
    try{
 const user= await User.signup(email,password)
-res.status(200).json({email,user})
+//create a token 
+const token = createToken(user._id)
+res.status(200).json({email,token})
    }catch(error){
      res.status(400).json({error:error.message})
    }
